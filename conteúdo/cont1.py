53b52f1e3ffd574ab3946d813307c607a14c5c83
enzocuchi-lab <enzo.cuchi@escola.pr.gov.br>
2026-03-05 11:42:45 -0300

    Create cont1.py

---
+++ conteúdo/cont1.py
@@ -0,0 +1,4 @@
+#bool = valores de verdadeiro e falso Ex: 7, 0, -4
+#int = para números inteiros Ex: 0.45, 7.6
+#float = para números reaisnou com virgúla Ex: True ou False(sempre usar comletra maiúscula)
+#str = para strings que são palavras caracteres ou textos Ex: 'ola', '7.5'
