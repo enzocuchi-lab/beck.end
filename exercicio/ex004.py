edfabe657edcde4456f6fc488ce54b92f18e61a1
enzocuchi-lab <enzo.cuchi@escola.pr.gov.br>
2026-03-09 08:31:39 -0300

    Create ex004.py

---
+++ exercicio/ex004.py
@@ -0,0 +1,7 @@
+num1 = int(input("Digite o primeiro número"))
+num2 = int(input("Digite o segundo número"))
+
+soma = num1 + num2
+
+#print('a soma entre {}'.format(num1)'é{}' .format(num2)' é {}' .format(soma))
+print("A soma entre {} e {} é {} ".format(num1,num2,soma))
