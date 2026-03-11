9a90b2390cebcf84d42263100ac25b4ca22db1f9
enzocuchi-lab <enzo.cuchi@escola.pr.gov.br>
2026-03-11 11:36:12 -0300

    Document arithmetic operators in cont4.py
    Add comments explaining basic arithmetic operators in Python

---
+++ conteúdo/cont4.py
@@ -0,0 +1,7 @@
+#O + é utilizado como operador de adição
+#O - é utilizado como sinal de subtração
+#O * é utilizado para fazer a multiplicação
+#/ é utilizada para fzaer a divisão
+#** é utilizado para fazer a exponenciação
+#// é utilizado para fazer a divisçao inteira
+#% é utiilizada para o resto da divisão
