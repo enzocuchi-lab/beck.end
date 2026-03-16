f72326e47bd33d3040313c22d72baa0eb0165f83
enzocuchi-lab <enzo.cuchi@escola.pr.gov.br>
2026-03-16 10:39:07 -0300

    Add operator precedence definitions in cont5.py

---
+++ conteúdo/cont5.py
@@ -0,0 +1,5 @@
+#Definição: é a ordem na qual os operadores serão executados 
+# 1. Expressões dentro de parenteses 
+# 2. Em seguida vem as potenciações 
+# 3. Em terceiro lugar vem a multiplicação, a divisão normal, a divisão inteira e o resto da divisão. No caso de ter mais de um deles vocẽ resolve quem aparecer primeiro 
+# 4. E por último a soma e a subtração  
