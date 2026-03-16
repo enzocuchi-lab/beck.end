b9a1522d18c5e3a7aadfd46090c2878e1e2d4479
enzocuchi-lab <enzo.cuchi@escola.pr.gov.br>
2026-03-16 10:47:16 -0300

    Update cont5.py

--- conteúdo/cont5.py
+++ conteúdo/cont5.py
@@ -3,3 +3,6 @@
 # 2. Em seguida vem as potenciações 
 # 3. Em terceiro lugar vem a multiplicação, a divisão normal, a divisão inteira e o resto da divisão. No caso de ter mais de um deles vocẽ resolve quem aparecer primeiro 
 # 4. E por último a soma e a subtração  
+#Para exponenciação podemos usar o pow(x,y) onde o número x é elevado ao número y
+#Para raiz quadrada podemos usar o **(1/2), elevando o número a 1/2 nós obtemos a raiz quadrada, para raiz cúbica **(1/3), e assim por diante 
+#Podemos usar os operadores em strings Ex: '='*5 vai ficar '====='
