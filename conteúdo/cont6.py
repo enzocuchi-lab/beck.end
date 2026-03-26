1b94ab04e06a43d6369395c34f52ee60321a12ee
enzocuchi-lab <enzo.cuchi@escola.pr.gov.br>
2026-03-26 08:21:25 -0300

    Create cont6.py

---
+++ conteúdo/cont6.py
@@ -0,0 +1,2 @@
+#Para imporatar bibliotecas e funcionalidades em Python utilisa-se o comando import seguido do nome da biblioteca   ou módulo que ira ser carregado nesse programa 
+#Para importar coisas específicas de uma bibliotecautilisa-se o comando fron seguido do nome da biblioteca seguido da palava import seguido da palavra que deseja importar
